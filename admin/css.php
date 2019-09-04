58e5e89156a97c291f012ac1b82acb30adddc31f
Ahmad Zaelani <myroot593@gmail.com>
2019-09-04 18:23:28 +0700

    Create css.php

---
+++ admin/css.php
@@ -0,0 +1,13 @@
+ <!-- Bootstrap core CSS-->
+    <link href="../properties/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template-->
+    <link href="../properties/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Page level plugin CSS-->
+    <link href="../properties/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="../properties/css/sb-admin.css" rel="stylesheet">
+    <link href="../properties/css/kostum-admin.css" rel="stylesheet">
+  </head>
