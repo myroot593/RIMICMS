65c5be084d276742ddeae123cd0ed9980399eb6a
Ahmad Zaelani <myroot593@gmail.com>
2019-09-04 18:18:57 +0700

    Create css.php

---
+++ css.php
@@ -0,0 +1,8 @@
+
+    <!-- Bootstrap core CSS -->    
+    <link href="properties/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+     <link href="properties/css/kostum.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="properties/css/4-col-portfolio.css" rel="stylesheet">
+
+  </head>
