3973bf1592ffe945f54ee1fcc7e093090a4039ba
Ahmad Zaelani <myroot593@gmail.com>
2019-09-04 18:30:13 +0700

    Create subcribeme.php

---
+++ admin/subcribeme.php
@@ -0,0 +1,26 @@
+<?php
+include ('head.php');
+include ('css.php');
+include ('navigasi.php');
+?>
+      <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+          <!-- Breadcrumbs-->
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="index.html">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Subcribe Me !</li>
+          </ol>
+
+          <!-- Page Content -->
+          <h3>Subcribe = Gratis</h3>
+          <a href="https://www.youtube.com/channel/UCNqYyG7I_uHC7JRxi-jZdtQ?sub_confirmation=1" class="btn btn-danger btn-md"><i class="fab fa-youtube fa-fw fa-1x"></i> SUBCRIBE</a>
+          <hr>
+          <div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/axsi6ysDcns?rel=0" allowfullscreen></iframe>
+</div>
+
+ <?php include('footer.php');?>      
