2e2d24df446ad501d594eb587d457d6c97083948
Ahmad Zaelani <myroot593@gmail.com>
2019-09-04 18:26:26 +0700

    Create footer.php

---
+++ admin/footer.php
@@ -0,0 +1,73 @@
+  </div>
+        <!-- /.container-fluid -->
+
+        <!-- Sticky Footer -->
+         <!-- Terima kasih  untuk tidak menghapus copyright -->
+        <footer class="sticky-footer">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright © RIMI CMS 2019. Proudly Powered by : <a href="https://root93.co.id" target="_balnk">root93</a></span>
+            </div>
+          </div>
+        </footer>
+
+      </div>
+      <!-- /.content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <a class="btn btn-primary" href="logout.php">Logout</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="../properties/vendor/jquery/jquery.min.js"></script>
+    <script src="../properties/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../properties/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Page level plugin JavaScript-->
+    <script type="text/javascript" src="../plugins/ckeditor2/ckeditor.js"></script>
+    <script src="../properties/vendor/chart.js/Chart.min.js"></script>
+    <script src="../properties/vendor/datatables/jquery.dataTables.js"></script>
+    <script src="../properties/vendor/datatables/dataTables.bootstrap4.js"></script>
+     <script>
+    $(document).ready(function() {
+        $('#dataTables-example').DataTable({
+            responsive: true
+        });
+    });
+    </script>
+    <!-- Custom scripts for all pages-->
+    <script src="../properties/js/sb-admin.min.js"></script>
+
+   <!-- Demo scripts for this page-->
+    <script src="../properties/js/demo/chart-area-demo.js"></script>
+    <script src="../properties/js/demo/chart-bar-demo.js"></script>
+    <script src="../properties/js/demo/chart-pie-demo.js"></script>
+    
+  </body>
+
+</html>
