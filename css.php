d2cb9c252eea1c32cb6e77b3eedb21b92c75e3c7
Ahmad Zaelani <myroot593@gmail.com>
2019-09-10 11:08:15 +0700

    Delete css.php

--- css.php
+++
@@ -1,8 +0,0 @@
-
-    <!-- Bootstrap core CSS -->    
-    <link href="properties/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-     <link href="properties/css/kostum.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="properties/css/4-col-portfolio.css" rel="stylesheet">
-
-  </head>
