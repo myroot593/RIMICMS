b3acf5d4132503433ae0ff393c194c79d90bde6c
Ahmad Zaelani <myroot593@gmail.com>
2019-09-10 11:07:37 +0700

    Update template.web.fungsi.php

--- function/template.web.fungsi.php
+++ function/template.web.fungsi.php
@@ -1,59 +1,129 @@
 <?php
-//Include properties
-  include ('databases/koneksi.php');
-  include ('function/public.web.fungsi.php');
-  include ('function/template.web.fungsi.php');
-?>
-<?php
-// Cek URL ID apakah berisi data atau tidak
-if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+function load_header(){
+echo '<!DOCTYPE html>
+	<html lang="id">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="CMS RIMI INDONESIA">
+    <meta name="author" content="Ahmad Zaelani">
+    <link rel="icon" href="http://localhost/cmsroot/properties/content/favicon.png">
+    <title>CMS RIMI</title>';
+}
+
+function load_css(){
+	echo '    
+	<!-- Bootstrap core CSS -->    
+    <link href="properties/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+     <link href="properties/css/kostum.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="properties/css/4-col-portfolio.css" rel="stylesheet">
+
+  </head>';
+}
+function load_navigasi(){
+	echo 
+	'<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="logo navbar-btn pull-left" href="index.php" title="Home">
+          <img src="properties/content/nav.png" alt="Home"> 
+        <a class="navbar-brand" href="index.php">CMS RIMI INDONESIA</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>';
+         
+            //memanggil fungsi tampil menu
+            $tampil_menu=tampil_menu();
+            if($tampil_menu){
+              if(num_rows($tampil_menu)>0){
+              while($data=mysqli_fetch_array($tampil_menu)){
+         
+        
+            //jika kategori single menu maka panggil seluruh bagian single menu
+            if($data['kategori_menu']=='single_menu'){
+         
+            echo '<li class="nav-item">
+              <a class="nav-link" href="'.$data[link_menu].'">'.$data[link_menu].'</a>
+            </li>';
     
-    if(!readmore_berita(trim($_GET["id"]))){
-                // Jika id tidak ditemukan maka alihkan ke halaman error
-                header("location: error");
-                exit();
-    }   
-    // Close connection
-    
-    }else{
-    // Jika URL ID di Address bar kosong maka arahkan ke halaman error
-    header("location: error");
-    exit();
+          //jika kategori dropdown menu mana panggil seluruh bag dropdown menu
+          }elseif($data['kategori_menu']=='dropdown_menu'){
+          
+          echo '<li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'.$data['nama_menu'].'</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">';
+                  
+                  //kemudian selanjutnya jika terdapat dapat sub menu didalam dropdown panggil submenunya
+                  $sub_menu=tampil_sub_menu($data['id']);
+                  //lakukan perulangan sub menu berdasarkan id menu parentnya
+                  while($sub=fetch($sub_menu)){
+                  
+                  echo '<a class="dropdown-item" href="'.$sub['link_menu'].'">'.$sub['nama_menu'].'</a>'; 
+                  } 
+           echo '</div>
+          </li>';
+            } //end dropdown 
+           
+                }//end while 
+                   }//end > 0 
+                }// end function tampil menu and create free result
+                free_result($tampil_menu);
+
+         
+          echo '</ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">';
+}
+function load_header_read(){
+	global $judul_berita;
+	echo '
+	<!DOCTYPE html>
+	<html lang="id">
+
+  	<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="CMS RIMI INDONESIA">
+    <meta name="author" content="Ahmad Zaelani">
+    <link rel="icon" href="../cmsrimi/properties/content/favicon.png">
+    <title>'.$judul_berita.' - CMS RIMI</title>';
+
+}
+function load_footer(){
+	echo '
+	</div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; CMS RIMI ROOT93 2019</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="properties/vendor/jquery/jquery.min.js"></script>
+    <script src="properties/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>';
 }
 ?>
-
-<?php
-  load_header_read();
-  load_css();
-  load_navigasi();
-?>
-<div class="row">
-<div id="content-wrapper">
-        <div class="container-fluid">
-          <!-- Breadcrumbs-->
-          <ol class="breadcrumb" style="margin-top: 15px;">
-            <li class="breadcrumb-item">
-              <a href="index.php">Kembali</a>
-            </li>
-            <li class="breadcrumb-item active"><?php echo $judul_berita; ?></li>
-          </ol>
-          <!-- Page Content -->
-         <figure class="figure">
-            <img src="content/<?php echo $gambar_berita; ?> " alt="<?php echo $judul_berita;?>" title="<?php echo $judul_berita;?>" class="figure-img img-fluid">
-            <figcaption class="figure-caption"><?php echo $judul_berita; ?></figcaption>
-            <figcaption class="figure-caption">Ditulis pada : <?php echo $tanggal_berita; ?> oleh : <?php echo $penulis_berita; ;?> | Label : <?php echo $kategori_berita; ;?></figcaption>
-    	 </figure>
-        <h3><?php echo $judul_berita; ?></h3>
-        <hr>
-        <?php echo htmlspecialchars_decode(htmlspecialchars_decode($isi_berita)); ?><br/>
-              <p class="small text-lef text-muted my-5">
-      
-    </p>
-  </div>
-</div>
-</div>
-<?php
-//footer
-load_footer();
-?>
-  
